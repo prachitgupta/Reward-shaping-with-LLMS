ef365e4914440d4d54817788abc5f7a5f510406b
prachitgupta <prachitgupta100@gmail.com>
2024-10-10 08:54:53 +0530

    edited test_claude

--- test_claude.py
+++ test_claude.py
@@ -355,7 +355,7 @@ if __name__ == ("__main__"):
     ##wrap video
     env = RecordVideo(env_llm.env, video_folder=video_path, episode_trigger=lambda ep: True)
 
-    for episode in trange(4, desc='Test episodes'):
+    for episode in trange(3, desc='Test episodes'):
         (obs, info), done, truncated = env.reset(), False, False
         episode_predictions = []  # Store predictions for the current episode
 
