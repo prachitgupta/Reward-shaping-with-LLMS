ff5c4df4e48b70f07a18e87daad7095b33d430be
prachitgupta <prachitgupta100@gmail.com>
2024-10-18 15:50:14 +0530

    edited test_claude

--- test_claude.py
+++ test_claude.py
@@ -308,7 +308,7 @@ class MyHighwayEnvLLM(gym.Env):
         # Part 6: Adding additional attention points and the final decision instruction
         safety_verification = '\nAttention points:\n\
         \t1.Safety is the main priority, You can stay IDLE or even Go slower but in no circumstance you should collide with lead vehicle.\n\
-        \t2.You are not supposed to change lane frequently only when its neccessary to keep the vehicle safe.\n\
+        \t2.You are not supposed to change lane frequently only when its neccessary to keep the vehicle safe. Before changing lane check safety like safe distance and speed fro other vehicles\n\
         \t3. Safety is a priority, but do not forget efficiency.\n\
         \t4. you should only make a decesion once you have verified safety with other vehicles otherwise make a new decesion and verify its safety from scratch\n \
         \t5. Your suggested action has to be one from the five listed actions - IDLE, SLOWER, FASTER, LANE_LEFT, LANE_RIGHT.\n\
