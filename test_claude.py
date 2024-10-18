2d70e51d75fa8fd848e499bab3a0c9450e83cb31
prachitgupta <prachitgupta100@gmail.com>
2024-10-18 10:54:26 +0530

    edited test_claude

--- test_claude.py
+++ test_claude.py
@@ -216,8 +216,9 @@ class MyHighwayEnvLLM(gym.Env):
         \t1.Safety is the main priority, You can maintain the same speed or even Go slower but in no circumstance you should collide with lead vehicle.\n\
         \t2.You are not supposed to change lane frequently only when its neccessary to keep the vehicle safe.\n\
         \t3. Ensure that your decision prioritizes both safety and efficiency; avoid decisions that lead to excessive idling which means maintaining the same speed.\n\
-        \t4. you should only make a decesion once you have verified safety with other vehicles otherwise make a new decesion and verify its safety from scratch\n \
-        \t5. Your action must be one of the five listed actions: IDLE, SLOWER, FASTER, LANE_LEFT, LANE_RIGHT.\n\
+        \t4. If your decesion is causing the vehicle to slow down excessively even if its completely safe to go fast, feel free to go with faster action, this is just to prevent excessive idling or deaccleration (you may check previous actiions to see if this is the case), in no circumstance this should compromise on safety \n \
+        \t5. you should only make a decesion once you have verified safety with other vehicles otherwise make a new decesion and verify its safety from scratch\n \
+        \t6. Your action must be one of the five listed actions: IDLE, SLOWER, FASTER, LANE_LEFT, LANE_RIGHT.\n\
         Your last action was ' + self.prev_action + '. Please recommend an action for the current scenario, only in this format: \'Final decision: <final decision>\'.\n'
 
         # Append the attention information to prompt2
@@ -441,7 +442,7 @@ if __name__ == ("__main__"):
 
     ##video folder path
     video_folder = "videos"
-    model_name = "test_claude/safe"
+    model_name = "test_claude/safe_efficient_fast"
     video_path = f"{video_folder}/{model_name}"
 
     ##wrap video
