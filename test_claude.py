a35fc4c9deef5c29d1ab5dbf9e03c3864f08a411
prachitgupta <prachitgupta100@gmail.com>
2024-10-21 18:35:16 +0530

    edited generator

--- test_claude.py
+++ test_claude.py
@@ -345,7 +345,7 @@ class MyHighwayEnvLLM(gym.Env):
         safety_verification = '\nAttention points:\n\
         \t1.Safety is the main priority, You can stay IDLE or even Go slower but in no circumstance you should collide with lead vehicle.\n\
         \t2.You are not supposed to change lane frequently only when its neccessary to keep the vehicle safe. Before changing lane check safety like safe distance and speed fro other vehicles\n\
-        \t3. Safety is a priority, but do not forget efficiency. You may Go faster only when theres no vehicle in lane or vehicles in your lane are EXCESSIVELY far off\n\
+        \t3. Safety is a priority, but do not forget efficiency. \n\
         \t4. you should only make a decesion once you have verified safety with other vehicles otherwise make a new decesion and verify its safety from scratch\n \
         \t5. Your suggested action has to be one from the five listed actions - IDLE, SLOWER, FASTER, LANE_LEFT, LANE_RIGHT.\n\
         Your last action was ' + self.prev_action + '.Please recommend action for the current scenario only in this format and DONT propound anything else other than \'Final decision: <final decision>\'.\n'
@@ -471,10 +471,10 @@ if __name__ == ("__main__"):
 
     for episode in trange(1, desc='Test episodes'):
         (obs, info), done, truncated = env.reset(), False, False
-        episode_predictions = []  # Store predictions for the current episode
-
+        episode_predictions = []  # Store predictions for the current episode 
+        #
         while not (done or truncated):
-            action = claude_query(env_llm,obs)   # Predict action using the random forest model 
+            action = claude_query(env_llm,obs) # Predict action using the random forest model 
             episode_predictions.append(action)  # Save the predicted action
 
             # Step in the environment
