bdf18b8255ae7052d6d4fad1e4c67bb4cabe84b2
prachitgupta <prachitgupta100@gmail.com>
2025-04-22 23:39:07 +0530

    rpundabout

--- roundabout/synthetic_data.py
+++ roundabout/synthetic_data.py
@@ -299,7 +299,7 @@ class MyRoundaboutEnvLLM(gym.Env):
         self.env = gym.make("roundabout-v0",render_mode='rgb_array', config= self.config)
         self.action_space = self.env.action_space
 
-        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(vehicleCount, 5), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(4, 5), dtype=np.float32)
 
     def find_smallest_positive(self, arr):
         smallest_positive = float('inf')
